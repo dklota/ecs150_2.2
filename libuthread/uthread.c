f56a0313aa7d5020e60fc0e26d32f9d9b1eb50f2
dlota <dlota@COE-CS-pc11.ou.ad3.ucdavis.edu>
2025-05-13 21:31:55 -0700

    made changes to uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -10,6 +10,7 @@
 #include "uthread.h"
 #include "queue.h"
 
+struct uthread_tcb *current_thread = NULL; // declared globally to use for func uthread_current
 
 enum state { // use enum to define the state
 	READY,
@@ -29,18 +30,48 @@ struct uthread_tcb {
 struct uthread_tcb *uthread_current(void)
 {
 	/* TODO Phase 2/3 */
+	return current_thread;
 }
 
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
-	// change the state from running to ready
+	struct uthread_tcb *curr = uthread_current();
+	struct uthread_tcb *next;
+
+	// change the state from running to ready, add to queue using enqueue
+	curr -> thread_state = READY;
+	queue_enqueue(ready_queue, curr);
+	//next = uthread_ctx_switch(curr, next)
 	
+	// dequeue next thread in queue, change the state from READY to RUNNING of next thread
+    queue_dequeue(ready_queue, (void **)&next);
+    next->thread_state = RUNNING; 
+
+    // switch context from the current to the next
+    uthread_set_current(next);
+    uthread_ctx_switch(&curr->context, &next->context);
 }
 
 void uthread_exit(void)
 {
 	/* TODO Phase 2 */
+	struct uthread_tcb *curr = uthread_current();
+	curr->thread_state = EXITED;
+
+	if (curr->stack)
+		uthread_ctx_destroy_stack(curr->stack);
+
+	free(curr);
+
+	struct uthread_tcb *next;
+	if (queue_dequeue(ready_queue, (void**)&next) == 0) {
+		next->thread_state = RUNNING;
+		current_thread = next;
+		setcontext(&next->context); // does not return
+	}
+
+	exit(0); // no threads left
 }
 
 int uthread_create(uthread_func_t func, void *arg)
@@ -68,6 +99,32 @@ int uthread_create(uthread_func_t func, void *arg)
 int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+	if (ready_queue != NULL)
+		return -1; // prevent reentry
+
+	ready_queue = queue_create();
+
+	// Create the main thread (current context)
+	struct uthread_tcb *main_thread = malloc(sizeof(struct uthread_tcb));
+	main_thread->thread_state = RUNNING;
+	main_thread->stack = NULL;
+	getcontext(&main_thread->context);
+	current_thread = main_thread;
+
+	// Create the first user thread
+	uthread_create(func, arg);
+
+	// Schedule the first thread
+	struct uthread_tcb *next;
+	queue_dequeue(ready_queue, (void**)&next);
+	next->thread_state = RUNNING;
+	current_thread = next;
+	uthread_ctx_switch(&main_thread->context, &next->context);
+
+	// Cleanup
+	free(main_thread);
+	return 0;
+
 }
 
 void uthread_block(void)
