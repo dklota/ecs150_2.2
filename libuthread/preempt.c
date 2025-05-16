b2c93d4fc4ecd45ab2a4c7fa8d3ad6f16a498da2
dklota <143158882+dklota@users.noreply.github.com>
2025-05-15 18:32:41 -0700

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,72 @@
  */
 #define HZ 100
 
+static struct sigaction original_action;
+static struct itimerval original_timer;
+static bool preemption_active = false;
+
+static void signal_handler(int signum)
+{
+    uthread_yield();
+}
+
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+	if (!preemption_active)
+        return;
+
+    sigset_t mask;
+    
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGVTALRM);
+    
+    sigprocmask(SIG_BLOCK, &mask, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+	if (!preemption_active)
+        return;
+
+    sigset_t mask;
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 void preempt_start(bool preempt)
 {
-	/* TODO Phase 4 */
+    if (!preempt)
+        return;
+    
+    struct sigaction sa;
+    struct itimerval timer;
+    
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = signal_handler;
+    sa.sa_flags = 0;
+    
+    sigaction(SIGVTALRM, &sa, &original_action);
+    
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 1000000 / HZ; 
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 1000000 / HZ;  
+    
+    setitimer(ITIMER_VIRTUAL, &timer, &original_timer);
+    
+    preemption_active = true;
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	if (!preemption_active)
+        return;
+    
+    setitimer(ITIMER_VIRTUAL, &original_timer, NULL);
+    sigaction(SIGVTALRM, &original_action, NULL);
+    
+    preemption_active = false;
 }
 
